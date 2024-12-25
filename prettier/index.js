1727aa124c4b4cd8e21bb9f7baf0ade8777f59fc
Eugene H. (idkgene) <webeugene@proton.me>
2024-12-25 12:20:55 +0300

    feat(prettier): add configuration schema and improvde code organization

--- prettier/index.js
+++ prettier/index.js
@@ -1,18 +1,21 @@
 /**
- * Some of Prettier's defaults can be overridden by an EditorConfig file. We
- * define those here to ensure that doesn't happen.
- *
- * See: https://github.com/prettier/prettier/blob/main/docs/configuration.md#editorconfig
+ * @type {import('prettier').Options}
+ * @see https://prettier.io/docs/en/options.html
  */
-const overridableDefaults = {
+const prettierConfig = {
+  /**
+   * Some of Prettier's defaults can be overridden by an EditorConfig file. We
+   * define those here to ensure that doesn't happen.
+   *
+   * @see https://github.com/prettier/prettier/blob/main/docs/configuration.md#editorconfig
+   */
   endOfLine: 'lf',
   tabWidth: 2,
   printWidth: 80,
   useTabs: false,
-};
 
-module.exports = {
-  ...overridableDefaults,
   singleQuote: true,
   plugins: ['prettier-plugin-packagejson'],
 };
+
+module.exports = prettierConfig
